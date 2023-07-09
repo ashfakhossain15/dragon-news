aeb98e47739e160791c04d02cee959e4694af4f3
ashfakhossain15 <ashfaksikder15@gmail.com>
2023-07-09 11:05:08 +0600

    left nav aside done

--- src/Component/Pages/Shared/Left Nav/LeftNav.jsx
+++ src/Component/Pages/Shared/Left Nav/LeftNav.jsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftNav = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5500/categories")
+      .then((res) => res.json())
+
+      .then((data) => setCategories(data))
+
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div>
-      <h2>Left Navigation</h2>
+    <div className="space-y-5">
+      <h2>All categories</h2>
+
+      <div className="divide-y">
+        {categories.map((category) => (
+          <p key={category.id} className="px-6 py-3 text-lg font-semibold hover:bg-slate-300 rounded-md transition duration-500 ">
+            <Link to={`/category/${category.id}`}>{category.name}</Link>
+          </p>
+        ))}
+      </div>
     </div>
   );
 };
